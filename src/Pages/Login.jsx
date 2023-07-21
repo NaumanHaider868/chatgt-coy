dce1099273d13eff3cbf72f733d4eef4a70833de
Nauman Haider <naumanhaider868@gmail.com>
2023-07-21 13:06:47 +0500

    apis

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
 import '../sass/Login.scss'
-import { Link } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import { Link,useNavigate } from 'react-router-dom'
+import axios from 'axios'
 
 function Login() {
+    const navigate = useNavigate();
+    const [email,setEmail] = useState('');
+    const [password,setPassword] = useState('');
+
+    let payload = {
+        email: email,
+        password: password
+    }
+    const handelLogin = () => {
+        axios.post('https://foodapis.techenablers.info/api/login',payload)
+        .then((res)=>{
+            console.log(res.data.data.token);
+            navigate('/content');
+            localStorage.setItem('token', res.data.data.token)
+        })
+    }
     return (
         <div className='login-page'>
             <div className='login-box'>
@@ -11,10 +29,11 @@ function Login() {
                 </div>
                 <div className='card'>
                     <div className='card-body'>
-                        <input type='email' placeholder='Email address' className='email-input' />
-                        <button className='btn btn-continue'>Continue</button>
+                        <input type='email' placeholder='Email address' name='email' className='email-input' onChange={(e)=>setEmail(e.target.value)} />
+                        <input type='password' placeholder='Password' name='password' className='email-input' onChange={(e)=>setPassword(e.target.value)} />
+                        <button className='btn btn-continue' onClick={handelLogin}>Continue</button>
                         <div className='sign-para'>
-                            <p>Don't have an account? <span><Link style={{color:'#10A37F'}} to='/newaccount'>Sign up</Link></span></p>
+                            <p>Don't have an account? <span><Link style={{ color: '#10A37F' }} to='/newaccount'>Sign up</Link></span></p>
                         </div>
 
                         {/* <div className='card-body-bottom'>
--- src/Pages/NewAccount.jsx
+++ src/Pages/NewAccount.jsx
@@ -1,10 +1,34 @@
 import React from 'react'
 import '../sass/Login.scss'
+import { useState, useEffect } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
+import axios from 'axios'
+
 function NewAccount() {
+    const [first_name, setFirstName] = useState('');
+    const [last_name, setLastName] = useState('');
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState();
+    const [phone, setPhone] = useState();
+    const [confirmPassword,setConfirmPassword] = useState()
     const navigate = useNavigate();
-    const nextPage = () => {
-        navigate('/telluseaboutyou')
+  
+    let payload = {
+        first_name: first_name,
+        last_name: last_name,
+        username: name,
+        email: email,
+        password: password,
+        phone: phone,
+        password_confirmation: confirmPassword
+    }
+    const handelSignUp = (e) => {
+        e.preventDefault();
+        axios.post('https://foodapis.techenablers.info/api/register', payload)
+            .then((resp) => {
+                navigate('/content')
+            })
     }
     return (
         <div className='login-page'>
@@ -15,9 +39,14 @@ function NewAccount() {
                 </div>
                 <div className='card'>
                     <div className='card-body'>
-                        <input type='email' placeholder='Email address' className='email-input' />
-                        <input type='password' placeholder='Password' className='email-input' />
-                        <button className='btn btn-continue' onClick={nextPage}>Continue</button>
+                        <input type='text' placeholder='First Name' className='email-input' onChange={(e) => setFirstName(e.target.value)} />
+                        <input type='text' placeholder='Last Name' className='email-input' onChange={(e) => setLastName(e.target.value)} />
+                        <input type='text' placeholder='User Name' className='email-input' onChange={(e) => setName(e.target.value)} />
+                        <input type='email' placeholder='Email address' className='email-input' onChange={(e) => setEmail(e.target.value)} />
+                        <input type='password' placeholder='Password' className='email-input' onChange={(e) => setPassword(e.target.value)} />
+                        <input type='text' placeholder='Password Comfiration' className='email-input' onChange={(e) => setConfirmPassword(e.target.value)} />
+                        <input type='text' placeholder='Phone' className='email-input' onChange={(e) => setPhone(e.target.value)} />
+                        <button className='btn btn-continue' onClick={handelSignUp}>Continue</button>
                         <div className='sign-para'>
                             <p>Already have an account? <span><Link style={{ color: '#10A37F' }} to='/'>Log in</Link></span></p>
                         </div>
