a06e2a34384daffc947fa1175723d4c67ed35d86
Nauman Haider <naumanhaider868@gmail.com>
2023-07-31 12:13:15 +0500

    changes

--- src/App.js
+++ src/App.js
@@ -1,27 +1,23 @@
 import Login from "./Pages/Login";
-import { Route,Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import NewAccount from "./Pages/NewAccount";
 import TellUs from "./Pages/TellUs";
 import Number from "./Pages/Number";
-import ContentCopy from './Pages/contentpages/ContentCopy'
-import Form from './Pages/contentpages/Form'
+import ContentCopy from './Pages/contentpages/ContentCopy.jsx'
+import Form from './Pages/contentpages/Form.jsx'
 
 function App() {
   return (
-    // <div className="wrapper">
-    //   <Content/>
-    // </div>
-      <Routes>
-        <Route path="/" element={<Login/>} />
-        <Route path='/newaccount' element={<NewAccount/>} />
-        <Route path='/telluseaboutyou' element={<TellUs/>} />
-        <Route path='/number' element={<Number/>} />
-        {/* <Route path='/content' element={<Content/>} /> */}
-        <Route path='/content' element={<ContentCopy/>} />
-        <Route path='/form' element={<Form/>} />
-      </Routes>
-    
-    );
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path='/newaccount' element={<NewAccount />} />
+      <Route path='/telluseaboutyou' element={<TellUs />} />
+      <Route path='/number' element={<Number />} />
+      <Route path='/content' element={<ContentCopy />} />
+      <Route path='/form' element={<Form />} />
+    </Routes>
+
+  );
 }
 
 export default App;
